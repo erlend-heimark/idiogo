5eaac825a86ef87cda69705ebe3a83d4ddb1efb7
erlendheimark <erlend.heimark@vipps.no>
2020-08-03 16:05:51 +0200

    better comment

--- api/server.go
+++ api/server.go
@@ -34,11 +34,11 @@ func New(path, port string, db mssql.Client, c extdadjokes.Client) *http.Server 
 	}
 }
 
-func handle(r chi.Router, method, path string, handleRequest func(r *http.Request) response) {
-	//Wrap handlers so that our handlers task is to return the response values. Enables us to have common functions for
-	//response writing. Also avoids the empty return pattern.
+//handle wraps the handler logic so that our handlers task is to return the response values. Enables us to have common functions for
+//response writing. Also avoids the empty return pattern.
+func handle(r chi.Router, method, path string, handleReq func(r *http.Request) response) {
 	r.Method(method, path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		response := handleRequest(r)
+		response := handleReq(r)
 		handleResponse(w, response)
 	}))
 }

7efa3a4b2cbd7a1e6831cd57ae0ddc4b9e02b908
erlendheimark <erlend.heimark@vipps.no>
2020-08-03 12:55:53 +0200

    Create response handler that wraps the handler code

--- pkg/api/handler.go
+++ pkg/api/handler.go
@@ -12,20 +12,19 @@ import (
 )
 
 //TODO Handlers sin hovedoppgave er å håndtere requests og response. Burde ha hjelpemetoder
-//TODO Strukturer slik at vi unngår empty returns. Viktig å ha en struktur som ikke er så lett å bomme på
 
 type Handler struct {
-	path string
-	port string
-	db mssql.Client
+	path    string
+	port    string
+	db      mssql.Client
 	fetcher externaldadjokes.Fetcher
 }
 
 func New(path, port string, db mssql.Client, fetcher externaldadjokes.Fetcher) Handler {
 	return Handler{
-		path: path,
-		port: port,
-		db: db,
+		path:    path,
+		port:    port,
+		db:      db,
 		fetcher: fetcher,
 	}
 }
@@ -53,78 +52,99 @@ func (h Handler) Init() {
 }
 
 func getDadJoke(db mssql.Client, fetcher externaldadjokes.Fetcher) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return createHandler(func(r *http.Request) apiResponse {
 		jokeID := chi.URLParam(r, "jokeId")
 		savedJoke, exists, err := db.GetDadJoke(r.Context(), jokeID)
 		if err != nil {
-			writeErrorResponse(w, err)
-			return
+			return apiResponse{
+				nil, 500, err,
+			}
 		}
 		if exists {
-			writeSuccessResponse(w, savedJoke)
-			return
+			return apiResponse{savedJoke, 200, nil}
 		}
 		extJoke, exists, err := fetcher.Get(jokeID)
 		if err != nil {
-			writeErrorResponse(w, err)
+			return apiResponse{
+				nil, 0, err,
+			}
 		}
 		if !exists {
-			w.WriteHeader(404)
-			return
+			return apiResponse{nil, 404, nil}
 		}
-
-		writeSuccessResponse(w, mapExternalDadjokeToInternal(extJoke))
-	}
+		return apiResponse{mapExternalDadjokeToInternal(extJoke), 200, nil}
+	})
 }
 
 func getRandomDadJoke(fetcher externaldadjokes.Fetcher) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return createHandler(func(r *http.Request) apiResponse {
 		d, err := fetcher.GetRandom()
 		if err != nil {
-			writeErrorResponse(w, err)
-			return
+			return apiResponse{
+				nil, 500, err,
+			}
 		}
-
-		writeSuccessResponse(w, mapExternalDadjokeToInternal(d))
-	}
+		return apiResponse{d, 200, nil}
+	})
 }
 
 func createDadJoke(db mssql.Client) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return createHandler(func(r *http.Request) apiResponse {
 		var dadJoke mssql.DadJoke
 		err := json.NewDecoder(r.Body).Decode(&dadJoke)
 		if err != nil {
-			writeErrorResponse(w, err)
-			//TODO Find a way to avoid these empty returns
-			return
+			return apiResponse{
+				nil, 500, err,
+			}
 		}
 
 		err = db.CreateDadJoke(r.Context(), dadJoke)
 		if err != nil {
+			return apiResponse{
+				nil, 500, err,
+			}
+		}
+		return apiResponse{nil, 200, nil}
+	})
+}
+
+type apiResponse struct {
+	data       interface{}
+	statusCode int
+	err        error
+}
+
+func createHandler(handleRequest func(r *http.Request) apiResponse) http.HandlerFunc {
+	//Wrap handlers so that our handlers task is to return the response values. Enables us to have common functions for
+	//response writing. Also avoids the empty return pattern.
+	return func(w http.ResponseWriter, r *http.Request) {
+		response := handleRequest(r)
+		handleResponse(w, response)
+	}
+}
+
+func handleResponse(w http.ResponseWriter, res apiResponse) {
+	w.WriteHeader(res.statusCode)
+	if res.err != nil {
+		writeErrorResponse(w, res.err)
+	}
+	if res.data != nil {
+		resp, err := json.Marshal(res)
+		if err != nil {
 			writeErrorResponse(w, err)
 			return
 		}
-		w.WriteHeader(201)
+		w.Header().Set("Content-Type", "application/json")
+		_, err = w.Write(resp)
+		if err != nil {
+			panic("oops")
+		}
 	}
 }
 
 func writeErrorResponse(w http.ResponseWriter, e error) {
 	w.WriteHeader(500)
 	_, err := w.Write([]byte(e.Error()))
-	if err != nil {
-		panic("oops")
-	}
-}
-
-func writeSuccessResponse(w http.ResponseWriter, response interface{}) {
-	resp, err := json.Marshal(response)
-	if err != nil {
-		writeErrorResponse(w, err)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	_, err = w.Write(resp)
 	if err != nil {
 		panic("oops")
 	}
